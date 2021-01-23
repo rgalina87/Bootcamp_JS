dfe7b7f48cc57f702404322b3baf6e3c42b1b8e6
rgalina87 <r.galina87@gmail.com>
2021-01-23 15:56:07 +0200

    ex xp

---
+++ Week_2/Day_2/Ex_XP/script.js
@@ -0,0 +1,33 @@
+//Exercise 1: Simple If/Else Statement
+let x = 5
+let y = 2
+
+if (x > y) {
+  console.log("x is greate")
+} else {
+  console.console.log("y is greate");
+}
+
+//Exercise 2: Chihuahua
+let newDog = "Chihuahua"
+
+console.log(newDog.length)
+
+console.log(newDog.toUpperCase())
+
+console.log(newDog.toLowerCase())
+
+if (newDog == "Chihuahua") {
+	console.log("I love Chihuahua, it’s my favorite dog breed");
+} else {
+	console.log("I dont care, I prefer cats")
+}
+
+//Exercise 3: Exen Or Not Even
+let number = prompt("Give a number")
+
+if (number % 2 == 0) {
+	alert('Even Number');
+} else {
+	alert('Odd Number');
+}
