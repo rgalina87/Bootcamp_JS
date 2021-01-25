069917c5a5019a60eea2e9c5e8f35313c2f3a7cc
rgalina87 <r.galina87@gmail.com>
2021-01-25 18:27:28 +0200

    !

--- Week_3/Day_2/Daily_Challenge/script.js
+++ Week_3/Day_2/Daily_Challenge/script.js
@@ -12,17 +12,3 @@ let person = document.getElementById('person');
         libButton.addEventListener('click', libIt);
 
 
-
-//         find the value of each of the input
-// write out a story that uses each of the values
-// Make sure you check the console for errors when you play the game.
-
-// function myFunction() {
-//   var x = document.forms[0];
-//   var txt = "";
-//   var i;
-//   for (i = 0; i < x.length; i++) {
-//     txt = txt + x.elements[i].value + "<br>";
-//   }
-//   document.getElementById("demo").innerHTML = txt;
-// }
